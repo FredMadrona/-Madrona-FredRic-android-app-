c3b55be4d2338d7e71034583dec34c09042d4159
fred madrona <Fredriccamasis.madrona@bicol-u.edu.ph>
2022-10-09 23:28:02 +0800

    commit the changes in dryrun.js

--- dryrun.js
+++ dryrun.js
@@ -1,14 +1,25 @@
-function getBrand(prefix){
-  console.log( prefix + this.brand);
+// function getBrand(prefix){
+//   console.log( prefix + this.brand);
+// }
+
+// let honda = {
+//   brand : 'Honda'
+// }
+
+// let audi = {
+//   brand: 'Audi'
+// }
+
+// getBrand.call(honda, "It's a ");
+// getBrand.call(audi, "It's an ");
+
+const person ={
+
+  name:`Fred`,
+  age: 23,
+  idNumber: 5515,
 }
 
-let honda = {
-  brand : 'Honda'
+for(const prop in person){
+  console.table(person[prop])
 }
-
-let audi = {
-  brand: 'Audi'
-}
-
-getBrand.call(honda, "It's a ");
-getBrand.call(audi, "It's an ");
