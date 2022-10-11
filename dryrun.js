1ad6eedded9d60b3594c55acfe36b5b87507db79
fred madrona <Fredriccamasis.madrona@bicol-u.edu.ph>
2022-10-11 14:52:49 +0800

    dryrun.js own property study

--- dryrun.js
+++ dryrun.js
@@ -18,8 +18,54 @@ const person ={
   name:`Fred`,
   age: 23,
   idNumber: 5515,
-}
+};
+const thirdEmployee = Object.create(person);
 
-for(const prop in person){
-  console.table(person[prop])
+thirdEmployee.info = "Third Employee";
+
+thirdEmployee.address =  "Tondo street Kilicao Daraga albay";
+
+
+for(const props in thirdEmployee){
+  console.log(props + " : " + thirdEmployee[props])
+}
+//try to implement Object.propertyIsEnumerable
+
+
+// console.log(person.propertyIsEnumerable(`age`));
+
+// Object.defineProperty(person, `ssn`,{
+// enumerable : true,
+// value: ` 123 934 533`
+
+
+// })
+
+// for(const prop in person){
+//   console.log(prop + " : " + person[prop])
+// }
+
+const employee = Object.create(person, {
+  job: {
+      value: 'JS Developer',
+      enumerable: true
+  }
+});
+
+
+
+
+
+
+const secondEmployee = Object.create(person, {
+  info :{
+    value: " Second Employee ",
+    enumerable: true
+  }
+});
+
+console.log(secondEmployee.info)
+
+for(const display in secondEmployee){
+  console.log(display + " : " + secondEmployee[display])
 }
