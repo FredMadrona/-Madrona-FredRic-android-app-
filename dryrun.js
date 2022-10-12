1ff599ec485aa585314c9cc1da338c27df53248c
fred madrona <Fredriccamasis.madrona@bicol-u.edu.ph>
2022-10-12 22:28:35 +0800

    Learned about the Object.assign() method

--- dryrun.js
+++ dryrun.js
@@ -13,22 +13,22 @@
 // getBrand.call(honda, "It's a ");
 // getBrand.call(audi, "It's an ");
 
-const person ={
+// const person ={
 
-  name:`Fred`,
-  age: 23,
-  idNumber: 5515,
-};
-const thirdEmployee = Object.create(person);
+//   name:`Fred`,
+//   age: 23,
+//   idNumber: 5515,
+// };
+// const thirdEmployee = Object.create(person);
 
-thirdEmployee.info = "Third Employee";
+// thirdEmployee.info = "Third Employee";
 
-thirdEmployee.address =  "Tondo street Kilicao Daraga albay";
+// thirdEmployee.address =  "Tondo street Kilicao Daraga albay";
 
 
-for(const props in thirdEmployee){
-  console.log(props + " : " + thirdEmployee[props])
-}
+// for(const props in thirdEmployee){
+//   console.log(props + " : " + thirdEmployee[props])
+// }
 //try to implement Object.propertyIsEnumerable
 
 
@@ -45,27 +45,68 @@ for(const props in thirdEmployee){
 //   console.log(prop + " : " + person[prop])
 // }
 
-const employee = Object.create(person, {
-  job: {
-      value: 'JS Developer',
-      enumerable: true
-  }
-});
+// const employee = Object.create(person, {
+//   job: {
+//       value: 'JS Developer',
+//       enumerable: true
+//   }
+// });
 
 
 
 
 
 
-const secondEmployee = Object.create(person, {
-  info :{
-    value: " Second Employee ",
-    enumerable: true
-  }
-});
+// const secondEmployee = Object.create(person, {
+//   info :{
+//     value: " Second Employee ",
+//     enumerable: true
+//   }
+// });
 
-console.log(secondEmployee.info)
+// console.log(secondEmployee.info)
 
-for(const display in secondEmployee){
-  console.log(display + " : " + secondEmployee[display])
-}
+// for(const display in secondEmployee){
+//   console.log(display + " : " + secondEmployee[display])
+// }
+
+
+// const person = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 25
+// };
+
+// person.sss = Object.defineProperty(person,{
+//   enumerable: false,
+//   value : '1234343'
+// })
+
+
+// const profile = Object.values(person);
+
+// console.log(profile);
+
+
+const ssn = Symbol(`ssn`);
+
+const person = {
+  firstName: 'John',
+    lastName: 'Doe',
+   age: 25,
+[ssn]: `123-3242345`
+  };
+
+  const person2 = {
+    firstName: 'John',
+      lastName: 'Madrona',
+     age: 26,
+  [ssn]: `123-3242345`
+    };
+
+  const kv = Object.entries(person)
+console.table(kv);
+
+const ibangPerson = Object.assign({}, person,person2);
+
+console.log(ibangPerson)
